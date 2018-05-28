8defe9ba78c1313edef3695f599491f6ae7f280c
Marcus Münger <marcus.munger@cybercom.com>
2018-05-28 18:53:36 +0200

    Added working dependencies

--- src/main/java/se/pro12/ironfox/rest/Parser.java
+++ src/main/java/se/pro12/ironfox/rest/Parser.java
@@ -1,15 +1,20 @@
 package se.pro12.ironfox.rest;
 
+import se.pro12.ironfox.model.Sheet;
 import se.pro12.ironfox.service.ParserService;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @ApplicationScoped
 @Path("/parse")
+@Produces(MediaType.APPLICATION_JSON)
 public class Parser {
     @Inject
     private ParserService parserService;
@@ -18,5 +23,11 @@ public class Parser {
     @Path("/status")
     public Response status() {
         return Response.ok().build();
+    }
+
+    @POST
+    @Path("")
+    public Response parse(Sheet sheet) {
+        return Response.ok("{\"valid\":" + parserService.isValid(sheet) + "}").build();
     }
 }
--- src/main/java/se/pro12/ironfox/service/ParserService.java
+++ src/main/java/se/pro12/ironfox/service/ParserService.java
@@ -1,10 +1,20 @@
 package se.pro12.ironfox.service;
 
+import se.pro12.ironfox.model.Sheet;
 import se.pro12.ironfox.util.Parser;
 
+import javax.ejb.Singleton;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Singleton
 public class ParserService {
 
-    public static List<Parser> parsers;
+    public static List<Parser> parsers = new ArrayList<>();
+
+    public boolean isValid(Sheet sheet) {
+        return parsers.stream().anyMatch(
+                parser -> parser.isValid(sheet));
+    }
 }
